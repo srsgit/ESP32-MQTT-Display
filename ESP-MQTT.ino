05b57451d88d830953022cae1562256f45443896
srsgit <git@theddle.co.uk>
2018-07-01 11:09:41 +0100

    Added WiFi Manager and saving configuration

--- ESP-MQTT.ino
+++ ESP-MQTT.ino
@@ -6,12 +6,19 @@
  achieve the same result without blocking the main loop.
 
 */
+// flag to format file syste and lose any saved config
+#define DEBUG_CLEAN
 
+#include <FS.h>
+#include <FSImpl.h>
+#include <vfs_api.h>
+#include "SPIFFS.h"
 #include <WiFi.h>
 #include <DNSServer.h>
 #include <WebServer.h>
 #include <WiFiManager.h>
 #include <PubSubClient.h>
+#include <ArduinoJson.h>
 
 #include <SPI.h>
 #include <Adafruit_GFX.h>
@@ -40,7 +47,23 @@ long lastMsg = 0;
 char msg[50];
 int value = 0;
 
-const char* mqtt_server = "192.168.1.8";
+//define your default values here, if there are different values in config.json, they are overwritten.
+//length should be max size + 1 
+char mqtt_server[40] = "";
+char mqtt_port[6] = "1883";
+//default custom static IP
+char static_ip[16] = "192.168.1.20";
+char static_gw[16] = "192.168.1.254";
+char static_sn[16] = "255.255.255.0";
+
+//flag for saving data
+bool shouldSaveConfig = false;
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
 
 void clearDisplay() {
   tft.setRotation(3);
@@ -106,19 +129,119 @@ void setup() {
   Serial.begin(115200);
   tft.begin();
   clearDisplay();
+  WiFi.mode(WIFI_STA);
+
+#ifdef DEBUG_CLEAN
+  //clean FS, for testing
+  //SPIFFS.format();
+#endif
+
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        json.printTo(Serial);
+        if (json.success()) {
+          Serial.println("\nparsed json");
+          strcpy(mqtt_server, json["mqtt_server"]);
+          strcpy(mqtt_port, json["mqtt_port"]);
+
+          if(json["ip"]) {
+            Serial.println("setting custom ip from config");
+            strcpy(static_ip, json["ip"]);
+            strcpy(static_gw, json["gateway"]);
+            strcpy(static_sn, json["subnet"]);
+            Serial.println(static_ip);
+
+          } else {
+            Serial.println("no custom ip in config");
+          }
+        } else {
+          Serial.println("failed to load json config");
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+
+  // The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  
+  WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
+  WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 5);
 
   WiFiManager wifiManager;
   //reset saved settings
   //wifiManager.resetSettings();
 
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+
+  wifiManager.setTimeout(180);
+
   //set callback that gets called when connecting to previous WiFi fails, and enters Access Point mode
   wifiManager.setAPCallback(configModeCallback);
 
+  //add all your parameters here
+  wifiManager.addParameter(&custom_mqtt_server);
+  wifiManager.addParameter(&custom_mqtt_port);
+
+  delay(3000);
   if (!wifiManager.autoConnect()) {
-    ESP.restart();
     clearDisplay();
-    delay(1000);
+    ESP.restart();
+    delay(5000);
   }
+
+  //read updated parameters
+  strcpy(mqtt_server, custom_mqtt_server.getValue());
+  strcpy(mqtt_port, custom_mqtt_port.getValue());
+
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["mqtt_server"] = mqtt_server;
+    json["mqtt_port"] = mqtt_port;
+
+    json["ip"]      = WiFi.localIP().toString();
+    json["gateway"] = WiFi.gatewayIP().toString();
+    json["subnet"]  = WiFi.subnetMask().toString();
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+
+    json.prettyPrintTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    //end save
+  }
+
+  Serial.println("\nlocal ip");
+  Serial.println(WiFi.localIP());
+  Serial.println(WiFi.gatewayIP());
+  Serial.println(WiFi.subnetMask());
+  Serial.println("MQTT Server");
+  Serial.println(mqtt_server);
 
   drawBackground();
 
